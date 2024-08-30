2ab6b2795d11bb96926ec0fc11157369ad11011b
Revti-Kale <revtiikale@gmail.com>
2024-08-31 00:32:06 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Nav from "./components/nav"
+
+const App = () => {
+  return (
+  <main>
+    <Nav/>
+  </main>
+  )
+}
+
+export default App
---
+++ src/components/nav.tsx
@@ -0,0 +1,30 @@
+import {BsSearch} from "react-icons/bs"
+import { CiUser } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import logo from '../logo/cookie.png'
+
+const Nav = () => {
+  return (
+    <div className="container hidden lg:block">
+      <div className="flex justify-between items-center p-8">
+        <img src={logo} alt="Logo" className="h-12"/>
+        <div className="relative w-full max-w-[500px]">
+            <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type="text"
+             placeholder="Search Product"></input>
+             <BsSearch className="absolute top-0 mt-4 right-0 mr-5 text-gray-500 size-{20}"/>
+        </div>
+        <div className="flex gap-4">
+            <div className="icon__wrapper">
+            <CiUser />
+            </div>
+            <div className="icon__wrapper relative">
+            <CiShoppingCart />
+            </div>
+        </div>
+
+      </div>
+        </div>
+  )
+}
+
+export default Nav
